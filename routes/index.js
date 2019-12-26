22c8e9fd0a1c456e3e18b277c15c418a8061874e
juliank <julian.kanjere@jskdezign.com>
2019-12-26 22:08:21 +0200

    Update tag lines

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var title = 'Tender Submission Registry. Tracing tender submissions from submission to contract awarding.';
+  var title = 'Tender Submission Registry. Tracing tender documents from submission to contract awarding.';
   var title_main = 'Tender Submission Registry.';
-  var title_sub = 'Tracing tender submissions from submission to contract awarding.';
+  var title_sub = 'Tracing tender documents from submission to contract awarding.';
   res.render('index', { title: title, title_main: title_main, title_sub: title_sub  });
 });
 
 /* GET test page. */
 router.get('/index2', function(req, res, next) {
-  var title = 'Tender Submission Registry. Tracing tender submissions from submission to contract awarding.';
+  var title = 'Tender Submission Registry. Tracing tender documents from submission to contract awarding.';
   var title_main = 'Tender Submission Registry.';
-  var title_sub = 'Tracing tender submissions from submission to contract awarding.';
+  var title_sub = 'Tracing tender documents from submission to contract awarding.';
   res.render('index2', { title: title, title_main: title_main, title_sub: title_sub  });
 });
 
