2bbb7924c1ad7d314ebafbcdd0e67776ccb382a0
juliank <julian.kanjere@jskdezign.com>
2019-12-26 21:02:40 +0200

    Add test route

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: title });
 });
 
+/* GET test page. */
+router.get('/index2', function(req, res, next) {
+  var title = 'Tender Submission Registry. Tracing tender submissions from submission to contract awarding.';
+  res.render('index2', { title: title });
+});
+
+
 module.exports = router;
