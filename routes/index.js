55a7e3932a130468919752997400d400d26c2d82
juliank <julian.kanjere@jskdezign.com>
2019-12-26 20:19:22 +0200

    Update home page title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var title = 'Tender Submission Registry. Tracing tender submissions from submission to contract awarding.';
+  res.render('index', { title: title });
 });
 
 module.exports = router;
