e5df07cb59c5350984edb0bf6de0cca433bb40c4
juliank <julian.kanjere@jskdezign.com>
2020-01-02 03:53:29 +0200

    Rename constants dict. Add triggerNotification function

--- public/javascripts/tendersubmissionregistry.js
+++ public/javascripts/tendersubmissionregistry.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
     const documentRegistryContractABI = [{"constant":false,"inputs":[{"name":"hash","type":"string"}],"name":"add","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"hash","type":"string"}],"name":"verify","outputs":[{"name":"dateAdded","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
 
 
-    var ROWNAMES = {
+    var CONSTANTS = {
                   HOME_TENDER_DOCS_MENU_BUTTON : "#homeTenderDocsMenuButton",
                   UPLOAD_TENDER_DOCS_MENU_BUTTON : "#uploadTenderDocsMenuButton",
                   VERIFY_TENDER_DOCS_MENU_BUTTON : "#verifyTenderDocsMenuButton",
                   LOADING : "#loadingRow",
-                  NOTIFICATION_BAR: "#notificationBarRow",
+                  NOTIFICATION_BAR_ROW: "#notificationBarRow",
                   HOW_IT_WORKS: "#howItWorksTenderDocsRow",
                   UPLOAD_TENDER_DOCS: "#uploadTenderDocsRow",
                   VERIFY_TENDER_DOCS: "#verifyTenderDocsRow",
@@ -17,122 +17,140 @@ $(document).ready(function() {
                   VERIFY_TENDER_DOCS_INPUT_FILE_ZIP: "#verifyTenderDocsInputFileZIP",
                   VERIFY_TENDER_DOCS_LABEL_FILE_ZIP: "#verifyTenderDocsLabelFileZIP",
                   UPLOAD_TENDER_DOCS_BUTTON_SUBMIT: "#uploadTenderDocsButtonSubmit",
-                  VERIFY_TENDER_DOCS_BUTTON_SUBMIT: "#verifyTenderDocsButtonSubmit"
+                  VERIFY_TENDER_DOCS_BUTTON_SUBMIT: "#verifyTenderDocsButtonSubmit",
+                  NOTIFICATION_BAR_DIV: "#divNotificationBar",
+                  SUCCESS: "success",
+                  ERROR: "error",
+                  WARNING: "warning"
                 };
 
-    $(ROWNAMES.HOME_TENDER_DOCS_MENU_BUTTON).click(function() { displayRow(ROWNAMES.HOW_IT_WORKS) });
-    $(ROWNAMES.UPLOAD_TENDER_DOCS_MENU_BUTTON).click(function() { displayRow(ROWNAMES.UPLOAD_TENDER_DOCS) });
-    $(ROWNAMES.VERIFY_TENDER_DOCS_MENU_BUTTON).click(function() { displayRow(ROWNAMES.VERIFY_TENDER_DOCS) });
-    $(ROWNAMES.UPLOAD_TENDER_DOCS_BUTTON_SUBMIT).click(uploadTenderZIP);
-    $(ROWNAMES.VERIFY_TENDER_DOCS_BUTTON_SUBMIT).click(verifyTenderZIP);
+    $(CONSTANTS.HOME_TENDER_DOCS_MENU_BUTTON).click(function() { displayRow(CONSTANTS.HOW_IT_WORKS) });
+    $(CONSTANTS.UPLOAD_TENDER_DOCS_MENU_BUTTON).click(function() { displayRow(CONSTANTS.UPLOAD_TENDER_DOCS) });
+    $(CONSTANTS.VERIFY_TENDER_DOCS_MENU_BUTTON).click(function() { displayRow(CONSTANTS.VERIFY_TENDER_DOCS) });
+    $(CONSTANTS.UPLOAD_TENDER_DOCS_BUTTON_SUBMIT).click(uploadTenderZIP);
+    $(CONSTANTS.VERIFY_TENDER_DOCS_BUTTON_SUBMIT).click(verifyTenderZIP);
 
-    displayRow(ROWNAMES.HOW_IT_WORKS);
+    displayRow(CONSTANTS.HOW_IT_WORKS);
 
     // Show/Hide a "loading" indicator when AJAX request starts/completes:
     $(document).on({
-        ajaxStart: function() { $(ROWNAMES.LOADING).show() }, //ajaxStart specifies a function to run when the first AJAX request begins
-        ajaxStop: function() { $(ROWNAMES.LOADING).hide() } //ajaxStop specifies a function to run when all AJAX requests have completed
+        ajaxStart: function() { $(CONSTANTS.LOADING).show() }, //ajaxStart specifies a function to run when the first AJAX request begins
+        ajaxStop: function() { $(CONSTANTS.LOADING).hide() } //ajaxStop specifies a function to run when all AJAX requests have completed
     });
-
-    $(ROWNAMES.VERIFY_TENDER_DOCS_INPUT_FILE_ZIP).on('change',function(){
+    
+    $(CONSTANTS.VERIFY_TENDER_DOCS_INPUT_FILE_ZIP).on('change',function(){
         //get the file name
         var fileName = $(this).val();
         //replace the "Choose a file" label
-        $(this).next(ROWNAMES.VERIFY_TENDER_DOCS_LABEL_FILE_ZIP).html(fileName);
+        $(this).next(CONSTANTS.VERIFY_TENDER_DOCS_LABEL_FILE_ZIP).html(fileName);
     });
 
-    $(ROWNAMES.UPLOAD_TENDER_DOCS_INPUT_FILE_ZIP).on('change',function(){
+    $(CONSTANTS.UPLOAD_TENDER_DOCS_INPUT_FILE_ZIP).on('change',function(){
         //get the file name
         var fileName = $(this).val();
         //replace the "Choose a file" label
-        $(this).next(ROWNAMES.UPLOAD_TENDER_DOCS_LABEL_FILE_ZIP).html(fileName);
+        $(this).next(CONSTANTS.UPLOAD_TENDER_DOCS_LABEL_FILE_ZIP).html(fileName);
     });
 
+    function triggerNotificationOpen(parentDivID, alertDivID, alertMessage, alertType) {
+      console.log("triggerNotificationOpen");
+      $(CONSTANTS.NOTIFICATION_BAR_ROW).show();
+      if (alertType === CONSTANTS.SUCCESS)
+        var divNotificationHtml = '<div id='+alertDivID+' class="alert alert-success fade in show"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button><strong>'+alertMessage+'</strong></div>';
+      else if (alertType === CONSTANTS.ERROR)
+         var divNotificationHtml = '<div id='+alertDivID+' class="alert alert-danger fade in show"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button><strong>'+alertMessage+'</strong></div>';
+      else if (alertType === CONSTANTS.WARNING)
+          var divNotificationHtml = '<div id='+alertDivID+' class="alert alert-warning fade in show"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button><strong>'+alertMessage+'</strong></div>';
+      console.log(divNotificationHtml)
+      $(parentDivID).html(divNotificationHtml);
+    };
+    
     function displayRow(rowName) {
         // Hide rows
-        $(ROWNAMES.LOADING).hide();
-        $(ROWNAMES.HOW_IT_WORKS).hide();
-        $(ROWNAMES.UPLOAD_TENDER_DOCS).hide();
-        $(ROWNAMES.VERIFY_TENDER_DOCS).hide();
+        $(CONSTANTS.LOADING).hide();
+        $(CONSTANTS.HOW_IT_WORKS).hide();
+        $(CONSTANTS.UPLOAD_TENDER_DOCS).hide();
+        $(CONSTANTS.VERIFY_TENDER_DOCS).hide();
+        $(CONSTANTS.NOTIFICATION_BAR_ROW).hide();
 
         // Display the passed in row
         $(rowName).show();
     }
 
     async function uploadTenderZIP() {
-        if ($(ROWNAMES.UPLOAD_TENDER_DOCS_INPUT_FILE_ZIP)[0].files.length == 0)
-            return showError("Please select a ZIP file to upload.");
+        if ($(CONSTANTS.UPLOAD_TENDER_DOCS_INPUT_FILE_ZIP)[0].files.length == 0){
+            var message_type = CONSTANTS.ERROR; //error or success
+            var message_description = "Please select a Tender Documents ZIP file to upload.";
+
+            triggerNotificationOpen(CONSTANTS.NOTIFICATION_BAR_DIV, '"divUploadTenderZIPAlert"', message_description, message_type);
+            return console.log(message_description);
+        }
 		if (window.ethereum)
 			try {
 				await window.ethereum.enable();
 			} catch (err) {
-                return showError("Access to your Ethereum account rejected.");
+                var message_type = CONSTANTS.ERROR; //error or success
+                var message_description = "Access to your Ethereum account rejected.";
+
+                triggerNotificationOpen(CONSTANTS.NOTIFICATION_BAR_DIV, '"divUploadTenderZIPAlert"', message_description, message_type);
+                return console.log(message_description);
 			}
         let fileReader = new FileReader();
         fileReader.onload = function() {
             let documentHash = sha256(fileReader.result);
-            if (typeof web3 === 'undefined')
-                return showError("Please install MetaMask to access the Ethereum Web3 injected API from your Web browser.");
+            if (typeof web3 === 'undefined'){
+                var message_type = CONSTANTS.ERROR; //error or success
+                var message_description = "Please install MetaMask to access the Ethereum Web3 injected API from your Web browser.";
+
+                triggerNotificationOpen(CONSTANTS.NOTIFICATION_BAR_DIV, '"divUploadTenderZIPAlert"', message_description, message_type);
+                return console.log(message_description);
+            }
+
             let contract = web3.eth.contract(documentRegistryContractABI).at(documentRegistryContractAddress);
             contract.add(documentHash, function(err, result) {
-                const flashRequest = new XMLHttpRequest();
-                flashRequest.open('post', '/flash');
-                flashRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-                var flash_page = "index";
-
                 if (err){
-                    var message_type = "error"; //error or success
+                    var message_type = CONSTANTS.ERROR; //error or success
                     var message_description = "Tender Submission Registry smart contract call failed: " + err;
 
-                    flashRequest.send(JSON.stringify({
-                        'message_type':message_type,
-                        'message_description': message_description,
-                        'message_page': flash_page
-                    }));
-
+                    triggerNotificationOpen(CONSTANTS.NOTIFICATION_BAR_DIV, '"divUploadTenderZIPAlert"', message_description, message_type);
                     return console.log("Smart contract call failed: " + err);
                 }
 
-                var message_type = "success"; //error or success
+                var message_type = CONSTANTS.SUCCESS; //error or success
                 var message_description = `Tender Documents ZIP file with hash ${documentHash} <b>successfully added</b> to the Tender Submission Registry (Blockchain).`;
 
-                flashRequest.send(JSON.stringify({
-                    'message_type':message_type,
-                    'message_description': message_description,
-                    'message_page': flash_page
-                }));
-
+                triggerNotificationOpen(CONSTANTS.NOTIFICATION_BAR_DIV, '"divUploadTenderZIPAlert"', message_description, message_type);
                 console.log(message_description);
             });
         };
-        fileReader.readAsBinaryString($(ROWNAMES.UPLOAD_TENDER_DOCS_INPUT_FILE_ZIP)[0].files[0]);
+        fileReader.readAsBinaryString($(CONSTANTS.UPLOAD_TENDER_DOCS_INPUT_FILE_ZIP)[0].files[0]);
     }
 
     function verifyTenderZIP() {
-        if ($(ROWNAMES.VERIFY_TENDER_DOCS_INPUT_FILE_ZIP)[0].files.length == 0)
-            return showError("Please select a ZIP file to verify.");
+        if ($(CONSTANTS.VERIFY_TENDER_DOCS_INPUT_FILE_ZIP)[0].files.length == 0){
+            var message_type = CONSTANTS.ERROR; //error or success
+            var message_description = "Please select a Tender Documents ZIP file to verify.";
+
+            triggerNotificationOpen(CONSTANTS.NOTIFICATION_BAR_DIV, '"divVerifyTenderZIPAlert"', message_description, message_type);
+            return console.log(message_description);
+        }
         let fileReader = new FileReader();
         fileReader.onload = function() {
             let documentHash = sha256(fileReader.result);
-            if (typeof web3 === 'undefined')
-                return showError("Please install MetaMask to access the Ethereum Web3 API from your Web browser.");
+            if (typeof web3 === 'undefined'){
+                var message_type = CONSTANTS.ERROR; //error or success
+                var message_description = "Please install MetaMask to access the Ethereum Web3 injected API from your Web browser.";
+
+                triggerNotificationOpen(CONSTANTS.NOTIFICATION_BAR_DIV, '"divVerifyTenderZIPAlert"', message_description, message_type);
+                return console.log(message_description);
+            }
             let contract = web3.eth.contract(documentRegistryContractABI).at(documentRegistryContractAddress);
             contract.verify(documentHash, function(err, result) {
-                const flashRequest = new XMLHttpRequest();
-                flashRequest.open('post', '/flash');
-                flashRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-                var flash_page = "index";
-
                 if (err){
-                    var message_type = "error"; //error or success
+                    var message_type = CONSTANTS.ERROR; //error or success
                     var message_description = "Tender Submission Registry smart contract call failed: " + err;
 
-                    flashRequest.send(JSON.stringify({
-                        'message_type':message_type,
-                        'message_description': message_description,
-                        'message_page': flash_page
-                    }));
-
+                    triggerNotificationOpen(CONSTANTS.NOTIFICATION_BAR_DIV, '"divVerifyTenderZIPAlert"', message_description, message_type);
                     return console.log(message_description);
                 }
 
@@ -140,29 +158,20 @@ $(document).ready(function() {
                 if (contractPublishDate > 0) {
                     let displayDate = new Date(contractPublishDate * 1000).toLocaleString();
 
-                    var message_type = "success"; //error or success
+                    var message_type = CONSTANTS.SUCCESS; //error or success
                     var message_description =`Tender Documents ZIP file with hash ${documentHash} is <b>valid<b>. Uploaded to Tender Submission Registry (Blockchain) on: ${displayDate}.`;
 
-                    flashRequest.send(JSON.stringify({
-                        'message_type':message_type,
-                        'message_description': message_description,
-                        'message_page': flash_page
-                    }));
-
+                    triggerNotificationOpen(CONSTANTS.NOTIFICATION_BAR_DIV, '"divVerifyTenderZIPAlert"', message_description, message_type);
                     console.log(message_description);
                 }
                 else
-                    var message_type = "error"; //error or success
+                    var message_type = CONSTANTS.ERROR; //error or success
                     var message_description =`Tender Documents ZIP file with hash ${documentHash} is <b>invalid</b>: not found in the Tender Submission Registry (Blockchain).`;
 
-                    flashRequest.send(JSON.stringify({
-                        'message_type':message_type,
-                        'message_description': message_description,
-                        'message_page': flash_page
-                    }));
+                    triggerNotificationOpen(CONSTANTS.NOTIFICATION_BAR_DIV, '"divVerifyTenderZIPAlert"', message_description, message_type);
                     return console.log(message_description);
             });
         };
-        fileReader.readAsBinaryString($(ROWNAMES.VERIFY_TENDER_DOCS_INPUT_FILE_ZIP)[0].files[0]);
+        fileReader.readAsBinaryString($(CONSTANTS.VERIFY_TENDER_DOCS_INPUT_FILE_ZIP)[0].files[0]);
     }
 });
