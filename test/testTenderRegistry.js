8a7c07709ff481845443772c76da99212b36b797
juliank <julian.kanjere@jskdezign.com>
2020-02-06 21:19:05 +0200

    Update tests. 4/6 passing.

--- test/testTenderRegistry.js
+++ test/testTenderRegistry.js
@@ -9,12 +9,13 @@ const TenderRegistry = artifacts.require('TenderRegistry');
 //     retrieving Tender Submission details for an existing submission using the ZIP File Hash works
 //     retrieving Tender Submission details for an non-existent submission using a Fake ZIP File Hash does not work
 
-contract('TenderRegistry', function () {
+contract('TenderRegistry', function (accounts) {
   // predefine parameters
   const ZIPFileDetails = 'ZIP File  test.zip (size 0.433993MB)';
   const ZIPFileHash = 'f149d75e984f1e919c4b896a0701637ff0260b834e1c18f3a9776c12fbf82311';
   const TenderSummary = '1003 - Train Data Center Operators';
-  const SupplierDetails = '{SubmitterFullName:"John Doe", SubmitterIdentificationNumber:"12345678",SupplierID:123}';
+  //const SupplierDetails = '{SubmitterFullName:"John Doe", SubmitterIdentificationNumber:"12345678",SupplierID:123}';
+  const SupplierDetails = '123';
   const SubmissionDate = 'Wed Feb 05 2020 00:09:56 GMT+0200 (South Africa Standard Time)';
   const SubmissionBlocktime = 1580854200;
   const IsSet = 1;
@@ -28,7 +29,8 @@ contract('TenderRegistry', function () {
   let initialTenderSubmissionCounter;
 
   // before running the tests, set the value of the initialTenderSubmissionCounter
-    before(async() => {
+  // before() The before function runs before the testing begins and it can be used to set the adequate variables to be used in each test.
+    before(async function () {
         // retrieve the deployed Tender Registry contract instance
         //TenderRegistryInstance = await TenderRegistry.deployed();
 
@@ -37,7 +39,10 @@ contract('TenderRegistry', function () {
 
           // get the initial number of Tender Submissions
         initialTenderSubmissionCounter = await TenderRegistryInstance.getTenderSubmissionsCount();
-    })
+    });
+
+
+  //it() It includes single units of tests, focused on testing specific aspects in a contract.
 
   it('should contain zero Tender Submissions in the beginning', async function () {
     // fetch instance of TenderRegistry contract
@@ -45,42 +50,40 @@ contract('TenderRegistry', function () {
     // get the number of Tender Submissions
     let tenderSubmissionCounter = await TenderRegistryInstance.getTenderSubmissionsCount()
     // check that there are no Tender Submissions initially
-    assert.equal(tenderSubmissionCounter, 0, 'initial number not equal to zero')
-  })
+    assert.equal(tenderSubmissionCounter, 0, 'initial number not equal to zero');
+  });
 
   it('should add a Tender Submission to the registry', async function () {
-    let TenderRegistryInstance = await TenderRegistry.deployed()
+    //let TenderRegistryInstance = await TenderRegistry.deployed();
     // register a Tender Submission to the registry
-    let tenderSubmissionIndex = await TenderRegistryInstance.registerTenderSubmission(
-      ZIPFileDetails,
-      ZIPFileHash,
-      TenderSummary,
-      SupplierDetails,
-      SubmissionDate
-    );
+    let tenderSubmissionIndex = await TenderRegistryInstance.registerTenderSubmission(ZIPFileDetails, ZIPFileHash, TenderSummary, SupplierDetails, SubmissionDate);
+
+    // now we'll check that the events are correct
+    // assert.equal(events.length, 1);
 
     //check return value is zero
-    assert.equal(tenderSubmissionIndex, initialTenderSubmissionCounter, 'Tender submission added successfully')
+    assert.equal(tenderSubmissionIndex, initialTenderSubmissionCounter, 'Tender submission was not added successfully');
 
-  })
+  });
 
   it('should increment Tender Submission hash array after adding to registry', async function () {
     //let TenderRegistryInstance = await TenderRegistry.deployed();
         // get the number of Tender Submissions
     let tenderSubmissionCounter = await TenderRegistryInstance.getTenderSubmissionsCount();
 
-    assert.equal(tenderSubmissionCounter.toNumber(), initialTenderSubmissionCounter + 1, 'Tender Submission was not successfully registered')
-  })
+    assert.equal(tenderSubmissionCounter.toNumber(), initialTenderSubmissionCounter + 1,
+        'Tender Submission was not successfully registered');
+  });
 
-  it('retrieve the Tender Hash', async function () {
+  it('should retrieve the Tender Hash', async function () {
     //retrieve from map using string array indexing
     //let TenderRegistryInstance = await TenderRegistry.deployed();
     let tenderSubmissionCounter = await TenderRegistryInstance.getTenderSubmissionsCount();
     let latestHash = await TenderRegistry.tenderHashes[tenderSubmissionCounter - 1];
 
     // check that it returns a match
-    assert.equal(latestHash, ZIPFileHash, 'Hash retrieved from Blockchain does not match latest submission')
-  })
+    assert.equal(latestHash, ZIPFileHash, 'Hash retrieved from Blockchain does not match latest submission');
+  });
 
   it('should verify a true Tender Submission from the registry', async function () {
     //let TenderRegistryInstance = await TenderRegistry.deployed()
@@ -94,7 +97,7 @@ contract('TenderRegistry', function () {
     assert.equal(tenderSubmissionInstance[2], TenderSummary, 'Tender Submission tender summary does not match');
     assert.equal(tenderSubmissionInstance[3], SupplierDetails, 'Tender Submission supplier details do not match');
     assert.equal(tenderSubmissionInstance[4], SubmissionDate, 'Tender Submission date does not match');
-  })
+  });
 
   it('should identify a false Tender Submission from the registry', async function () {
     //let TenderRegistryInstance = await TenderRegistry.deployed();
@@ -103,7 +106,7 @@ contract('TenderRegistry', function () {
 
     //we expect the IsSet variable to be undefined for a fake Hash
     assert(tenderSubmissionInstance[7] === undefined, 'FakeZIPFileHash returned a Tender Submission which is not expected behaviour');
-  })
+  });
 
 
-})
+});
